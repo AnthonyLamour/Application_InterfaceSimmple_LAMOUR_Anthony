2a81100355fd2edb9f6ccb4be548c3d7d309a156
Anthony LAMOUR <a.lamour@ludus-academie.com>
2018-10-26 15:54:34 +0200

    GitFinal

--- app/src/main/java/com/example/lamou/applicationinterfacesimplelamouranthony/MainActivity.java
+++ app/src/main/java/com/example/lamou/applicationinterfacesimplelamouranthony/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import java.text.SimpleDateFormat;
@@ -27,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView MaDate;
     private RadioButton H;
     private RadioButton F;
+    private TextView NbHeure;
+    private Button Res;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,11 +41,44 @@ public class MainActivity extends AppCompatActivity {
         MaDate= (TextView) findViewById(R.id.Date);
         Nom= (TextView) findViewById(R.id.NOM);
         Prenom= (TextView) findViewById(R.id.Prenom);
+        NbHeure= (TextView) findViewById(R.id.Heure);
         datetext=new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
         MaDate.setText(datetext);
         Mail = (TextView) findViewById(R.id.Mail) ;
         H = (RadioButton) findViewById(R.id.Homme) ;
         F = (RadioButton) findViewById(R.id.Femme) ;
+        Res = (Button) findViewById(R.id.Res) ;
+        Res.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Nom.setText("");
+                Prenom.setText("");
+                NbHeure.setText("");
+                H.setChecked(false);
+                F.setChecked(false);
+                Log.i("RAZ","Raz executer");
+            }
+        });
+        NbHeure.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                try {
+                    int num = Integer.parseInt(NbHeure.getEditableText().toString());
+                    if (num <= 5) {
+                        int duration = Toast.LENGTH_SHORT;
+                        Toast.makeText(MainActivity.this, "Correct", duration).show();
+                    } else if (num > 5 && num<=10) {
+                        int duration = Toast.LENGTH_SHORT;
+                        Toast.makeText(MainActivity.this, "Attention", duration).show();
+                    } else if (num < 10) {
+                        int duration = Toast.LENGTH_LONG;
+                        Toast.makeText(MainActivity.this, "Addict", duration).show();
+                    }
+                }catch (NumberFormatException e) {
+                    Log.i("",NbHeure.getEditableText().toString()+" is not a number");
+                }
+            }
+        });
         H.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
